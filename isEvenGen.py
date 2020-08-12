9db4b669258f557ead7a2a3051b601a72a17ecf0
michael <watty.wat@me.com>
2020-08-13 00:02:44 +0100

    initial commit

---
+++ isEvenGen.py
@@ -0,0 +1,19 @@
+file = open("isEven.py", "w")
+file.write("def isEven(nbr):\n    if(nbr == '0'): return False\n")
+
+output = ""
+
+for i in range(1,10000000):
+    truefalse = (i%2==0)
+    file.write("    if(nbr == '")
+    file.write(str(i))
+    file.write("'): return ")
+    file.write(str(truefalse))
+    file.write("\n")
+    file.flush()
+
+file.write(output)
+file.write("\n\nnbr = input(\"Enter number:\")\nprint(isEven(nbr))")
+file.close()
+
+print("done")
